320b2dd02a8ac9fa72278be874776aca5a1c5de9
Rob <tehrhawb@gmail.com>
2017-06-30 12:43:14 -0400

    Split Name

--- TeamThing/TeamThing/Person.cs
+++ TeamThing/TeamThing/Person.cs
@@ -8,7 +8,8 @@ namespace TeamThing
 {
 	class Person
 	{
-		public string name { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
 		public string address { get; set; }
 	}
 }
